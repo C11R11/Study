cbd03ce7d3d577e026c975bbe8f1ccd59df5daf1
Cristian Rodriguez <cristian.rodriguez@maptek.cl>
2024-05-13 23:02:57 -0400

    mongodb on docker and mongoose first attempt

--- Architecture/Rest/expressTypescript/README.md
+++ Architecture/Rest/expressTypescript/README.md
@@ -17,3 +17,38 @@ Also added:
 
 - Testing practices with Jest and Superuser
 - Docker mongo container (to do)
+
+# Configuring mongo db and docker
+
+## Create docker container
+
+https://github.com/themattman/mongodb-raspberrypi-docker
+
+follow this one as well
+
+https://www.mongodb.com/developer/products/mongodb/mongodb-on-raspberry-pi/
+
+
+## Connect with volume
+
+```bash
+#Run
+docker run -d -v /home/pi/data/mongodbexpress/:/data/db -p 27017:27017 mongodb-raspberrypi4-unofficial-r7.0.4 --auth
+
+#Connect
+docker exec -it b3c6e4fd4abc bash
+
+#Create user to connect from external
+use admin
+db.createUser( { user: "admin", pwd: "SUPERSECRETPASSWORD", roles: [ "userAdminAnyDatabase", "dbAdminAnyDatabase","readWriteAnyDatabase"] } )
+
+#See the users created 
+use admin
+db.system.users.find()
+
+#Auth
+db.auth("user", "password")
+
+#Conner with compass
+mongodb://<user>:<pass>@<remote_ip>:27017/
+```
--- Architecture/Rest/expressTypescript/src/app.ts
+++ Architecture/Rest/expressTypescript/src/app.ts
@@ -1,22 +1,46 @@
 import express from "express";
 import { json } from "express";
 import morgan from "morgan";
-import albumsRouter from "./routes/albumRouter"
-import usersRouter from "./routes/userRouter"
+import albumsRouter from "./routes/albumRouter";
+import usersRouter from "./routes/userRouter";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config({ path: "./config.env" });
+const db = async () =>
+  await mongoose.connect(process.env.DATABASE_CONECCTION_DOCKER);
+db().catch((err) => console.error(err));
+
+console.log(process.env.DATABASE_CONECCTION_DOCKER);
+
+const kittySchema = new mongoose.Schema({
+  name: { type: String, unique: true, required: [true, "must have name"] },
+  age: { type: Number, unique: true, required: [true, "must have age"] },
+});
+
+const Kitten = mongoose.model("Kitten", kittySchema);
+
+const silence = new Kitten({ name: "Silence2", age:2});
+console.log(silence.name); // 'Silence'
+silence
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => console.error(err));
 
 const app = express();
 
-app.use(morgan("dev"))
+app.use(morgan("dev"));
 
 //this one here is an important middleware to capture the users json data
 app.use(json());
 
 //custom middleware
-app.use((req, res, next)=>
-  {
-    console.log("Custom middleware :)")
-    next()
-  })
+app.use((req, res, next) => {
+  console.log("Custom middleware :)");
+  next();
+});
 
 function ApiStatus(req, res) {
   res.status(200).json({
